626c708c3889257d95110a7592c522a53e9b2def
Manoj Kumar Mittal <M1023845@mindtree.com>
2018-09-13 17:27:27 +0530

    inital version

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=intelligentagent-Web-Deploy.pubxml -p:Password=pfdqwFcomQxpi3FZbLZA5hs7o71ZSzulvhRAibgmjHXmR6luS6D09S0A3lXo
+
